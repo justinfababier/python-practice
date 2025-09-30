092abe8e3cca0b5d7bd939bdc4a1dede4a858588
Justin Fababier <justinfababier@gmail.com>
2025-09-30 18:15:33 +0900

    Initial commit

---
+++ forward_kinematics.py
@@ -0,0 +1,59 @@
+import numpy as np
+import modern_robotics as mr
+
+def forward_kinematics(joints) -> list[float]:
+    """
+    Forward kinematics of ReactorX-150 Robot Arm.
+    """
+    # Input: joint angles [joint1, joint2, joint3]
+    # Output: the position of end effector [x, y, z]
+
+    # Define link lengths
+    link1z = 0.065
+    link2z = 0.039
+    link3x = 0.050
+    link3z = 0.150
+    link4x = 0.150
+    
+    # Define joint angles
+    joint1 = joints[0]
+    joint2 = joints[1]
+    joint3 = joints[2]
+
+    # Define screw axes
+    S1 = np.array([0, 0, 1, 0, 0, 0])
+    S2 = np.array([0, 1, 0, -(link1z + link2z), 0, 0])
+    S3 = np.array([0, -1, 0, (link1z + link2z + link3z), 0, -link3x])
+
+    # Compute the exponential of the screw coordinates
+    S1_skewsym = mr.MatrixExp6(mr.VecTose3(S1 * joint1))
+    S2_skewsym = mr.MatrixExp6(mr.VecTose3(S2 * joint2))
+    S3_skewsym = mr.MatrixExp6(mr.VecTose3(S3 * joint3))
+
+    M = np.array([[1, 0, 0, (link3x + link4x)],
+                  [0, 1, 0, 0],
+                  [0, 0, 1, (link1z + link2z + link3z)],
+                  [0, 0, 0, 1]])
+
+    # Calculate end effector transformation matrix
+    T_04 = S1_skewsym @ S2_skewsym @ S3_skewsym @ M
+
+    # Extract end effector position from the transformation matrix
+    print()
+    x = T_04[0, 3]
+    y = T_04[1, 3]
+    z = T_04[2, 3]
+
+    # Print transformation matrix
+    print(f"T_04 = \n{T_04}")
+
+    return [x, y, z]
+
+def main():
+    joints = [0, 0, 0]
+    fk = forward_kinematics(joints)
+    print(f"Joint angles (rad) = {joints}")
+    print(f"End effector position (x, y, z) = {fk}")
+
+if __name__ == "__main__":
+    main()
---
+++ inverse_kinematics.py
@@ -0,0 +1,52 @@
+import numpy as np
+
+def arccos_clamped(x):
+    """
+    Clamp arccos value to [-1, 1]
+    """
+    return np.arccos(np.clip(x, -1.0, 1.0))
+
+def inverse_kinematics(position) -> list[float]:
+    """
+    Inverse kinematics of ReactorX-150 Robot Arm.
+    """
+    # Input: position of end effector [x, y, z]
+    # Output: joint angles [joint1, joint2, joint3]
+
+    link1z = 0.065
+    link2z = 0.039
+    link3x = 0.050
+    link3z = 0.150
+    link4x = 0.150
+
+    x = position[0]
+    y = position[1]
+    z = position[2]
+
+    # Solving for joint 1
+    joint1 = np.atan2(y, x) # Location of joint 1
+
+    # Solving for joint 2
+    r = np.sqrt((x ** 2) + (y ** 2))
+    a = np.sqrt((r ** 2) + ((z - link1z - link2z) ** 2))
+    gamma = arccos_clamped(r / a)
+    k = 0.1581
+    alpha = arccos_clamped(link3z / k)
+    beta_1 = arccos_clamped(((a ** 2) + (k ** 2) - (link4x ** 2)) / (2 * a * k))
+    joint2 = (np.pi / 2) - alpha - beta_1 - gamma
+
+    # Solving for joint 3
+    eta = np.atan(link3z / link3x)
+    beta_2 = arccos_clamped(((k ** 2) + (link4x ** 2) - (a ** 2)) / (2 * k * link4x))
+    joint3 = beta_2 - (np.pi - eta)
+    
+    return [joint1, joint2, joint3]
+
+def main():
+    position = [0.2, 0.0, 0.254]
+    ik = inverse_kinematics(position)
+    print(f"Joint Angles (rad): {ik}")
+    print(f"End effector position (x, y, z): {position}")
+
+if __name__ == "__main__":
+    main()
